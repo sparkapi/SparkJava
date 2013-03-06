305b34bdc1db9c862cb69d61749ca054df9a58f3
David Ragones <dragones@dreamcommerce.co>
2013-03-05 22:28:54 -0800

    testIsHybridAuthorized

--- src/test/java/com/sparkplatform/api/SparkAPITest.java
+++ src/test/java/com/sparkplatform/api/SparkAPITest.java
@@ -1,7 +1,23 @@
 package com.sparkplatform.api;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 
 public class SparkAPITest {
+	
+	private static final String hybridAuthorizationURLBase = 
+			"https://sparkplatform.com/oauth2/callback?";
+	private static final String hybridAuthorizationURLPass = 
+			hybridAuthorizationURLBase + "openid.mode=id_res&openid.spark.code=foobar";
+	private static final String hybridAuthorizationURLFail = 
+			"https://sparkplatform.com/ticket";
+	
+	@Test
+	public void testIsHybridAuthorized() {
+		assertNotNull(SparkAPI.isHybridAuthorized(hybridAuthorizationURLPass));
+		assertNull(SparkAPI.isHybridAuthorized(hybridAuthorizationURLFail));
+	}
 	
 }
